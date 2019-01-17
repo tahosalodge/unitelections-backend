5fcf3a793465d2125ea0f7019ebc1aaf5965468a
Kevin McKernan <kevin@mckernan.in>
2019-01-16 19:20:51 -0700

    add error for missing slack env var

--- src/utils/slack.ts
+++ src/utils/slack.ts
@@ -1,5 +1,9 @@
 import { IncomingWebhook } from '@slack/client';
 import config from 'utils/config';
+
+if (!config.slackUrl) {
+  throw new Error('Missing Slack webhook url.');
+}
 const slack = new IncomingWebhook(config.slackUrl);
 
 export default slack;
