52af30b5625403218fb7f7439c0ffdd25a2f40c7
Kevin McKernan <kevin@mckernan.in>
2019-02-03 13:27:21 -0700

    fix typing for define, move model names to constants

--- src/modules/user/roles.ts
+++ src/modules/user/roles.ts
@@ -1,31 +1,40 @@
-import { AbilityBuilder } from '@casl/ability';
+import { AbilityBuilder, AbilityBuilderParts } from '@casl/ability';
 import { TokenUser } from 'user/model';
 
+const LODGE = 'Lodge';
+const USER = 'User';
+const UNIT = 'Unit';
+const ELECTION = 'Election';
+const CANDIDATE = 'Candidate';
+
 export const defineAbilitiesFor = (user?: TokenUser) =>
-  AbilityBuilder.define((can, cannot) => {
-    can('create', 'User');
-    can('read', 'Lodge');
+  AbilityBuilder.define((can: AbilityBuilderParts['can']) => {
+    can('create', USER);
+    can('read', LODGE);
 
     if (user) {
       if (user.isAdmin) {
-        can('manage', 'Lodge');
-        can('manage', 'User');
-        can('manage', 'Unit');
-        can('manage', 'Election');
-        can('administer', 'User');
+        can('manage', LODGE);
+        can('manage', USER);
+        can('manage', UNIT);
+        can('manage', ELECTION);
+        can('manage', CANDIDATE);
+        can('administer', USER);
       } else {
         const ids = user.belongsTo.map(related => related.organization);
-        const manageableIds = user.belongsTo.filter(related => related.canManage).map(related => related.organization);
-        can('read', 'Lodge', { _id: { $in: ids } });
-        can('read', 'Unit', { _id: { $in: ids } });
-        can('read', 'Unit', { chapter: { $in: ids } });
-        can('manage', 'Unit', { chapter: { $in: manageableIds } });
-        can('create', 'Unit');
-        can('read', 'Election', { _id: { $in: ids } });
-        can('read', 'Election', { chapter: { $in: ids } });
-        can('manage', 'Election', { chapter: { $in: manageableIds } });
-        can('create', 'Election');
-        can('manage', 'User', { _id: user.userId });
+        const manageableIds = user.belongsTo
+          .filter(related => related.canManage)
+          .map(related => related.organization);
+        can('read', LODGE, { _id: { $in: ids } });
+        can('read', UNIT, { _id: { $in: ids } });
+        can('read', UNIT, { chapter: { $in: ids } });
+        can('manage', UNIT, { chapter: { $in: manageableIds } });
+        can('create', UNIT);
+        can('read', ELECTION, { _id: { $in: ids } });
+        can('read', ELECTION, { chapter: { $in: ids } });
+        can('manage', ELECTION, { chapter: { $in: manageableIds } });
+        can('create', ELECTION);
+        can('manage', USER, { _id: user.userId });
       }
     }
   });
