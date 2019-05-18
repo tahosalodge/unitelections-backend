22c68b5d2a9813c54e202251336e10cb1d90caa5
Kevin McKernan <kevin@mckernan.in>
2019-05-18 13:18:16 -0600

    remove scale from now deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,6 @@ if [ -e now.json ]
 then
     yarn now --force --token $ZEIT_TOKEN
     yarn now alias --token $ZEIT_TOKEN
-    yarn now scale elections-api.tahosa.co all 1 --token $ZEIT_TOKEN
     yarn now rm elections-api-production --token $ZEIT_TOKEN --safe --yes
 else
     echo "No environment to deploy, exiting."
